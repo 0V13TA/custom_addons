a1bedd4b47e9ee7401d6d9a830cf8fbfa1cee89e
0V13TA <vincentpaulomorogbe6@gmail.com>
2026-02-19 14:14:10 +0100

    Fixed somethings

--- enterprise_theme/__manifest__.py
+++ enterprise_theme/__manifest__.py
@@ -13,7 +13,7 @@ Long description of module's purpose
     "category": "Uncategorized",
     "version": "0.1",
     # any module necessary for this one to work correctly
-    "depends": ["base"],
+    "depends": ["base", "web"],
     # always loaded
     "data": [
         # 'security/ir.model.access.csv',
